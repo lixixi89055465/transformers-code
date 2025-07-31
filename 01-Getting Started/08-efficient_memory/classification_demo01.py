29b40820a6b5f7a43e3ce3fc5899c196753d34aa
lixixi89055465 <nanji1850094299@outlook.com>
2025-07-31 23:23:51 +0800

    dataset study

--- 01-Getting Started/08-efficient_memory/classification_demo01.py
+++ 01-Getting Started/08-efficient_memory/classification_demo01.py
@@ -10,11 +10,13 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trai
 from datasets import load_dataset
 
 # step2 加载数据集
-dataset = load_dataset(
-    'csv',
-    data_files='/home/nanji/workspace/transformers-code/01-Getting Started/06-evaluate/ChnSentiCorp_htl_all.csv',
-    split='train'
-)
+# dataset = load_dataset(
+#     'csv',
+#     data_files='/home/nanji/workspace/transformers-code/01-Getting Started/07-trainer/ChnSentiCorp_htl_all.csv',
+#     split='train'
+# )
+dataset = load_dataset("csv", data_files="./ChnSentiCorp_htl_all.csv", split="train")
+dataset = dataset.filter(lambda x: x["review"] is not None)
 
 # step3 划分数据集
 datasets = dataset.train_test_split(test_size=0.1)
@@ -26,24 +28,14 @@ import torch
 tokenizer = AutoTokenizer.from_pretrained('/home/nanji/workspace/rbt3')
 
 
+
 def process_function(examples):
-    if examples is None or "review" not in examples or "label" not in examples:
-        print(examples)
-    if examples['review'] is None or len(examples['review']) == 0:
-        print(examples)
-    try:
-        tokenized_examples = tokenizer(examples["review"], max_length=128, truncation=True)
-    except:
-        for i in examples['review']:
-            if i:
-                print(i)
-            else:
-                print(i)
+    tokenized_examples = tokenizer(examples["review"], max_length=128, truncation=True)
     tokenized_examples["labels"] = examples["label"]
     return tokenized_examples
 
 
-tokenized_datasets = datasets.map(process_function, batched=False, remove_columns=datasets["train"].column_names)
+tokenized_datasets = datasets.map(process_function, batched=True, remove_columns=datasets["train"].column_names)
 print(tokenized_datasets)
 # step5 创建模型
 model = AutoModelForSequenceClassification.from_pretrained('/home/nanji/workspace/rbt3')
