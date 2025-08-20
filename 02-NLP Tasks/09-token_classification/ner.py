e223c41cfa0a2696012be4e21f5fdbbbba854ca8
lixixi89055465 <nanji1850094299@outlook.com>
2025-08-20 23:29:46 +0800

    ner datasets

--- 02-NLP Tasks/09-token_classification/ner.py
+++ 02-NLP Tasks/09-token_classification/ner.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
 
 # ner_datasets=load_dataset("peoples_daily_ner",cache_dir='./data')
 # ner_datasets = load_dataset("/home/nanji/workspace/people-daily-ner", cache_dir='/home/nanji/workspace/people-daily-ner')
-ner_datasets=load_dataset("./ner_data",cache_dir='./ner_data')
+ner_datasets = load_dataset("./ner_data", cache_dir='./ner_data')
 
 print(ner_datasets)
 print('0' * 100)
@@ -15,17 +15,11 @@ print(ner_datasets['train'][0])
 print('1' * 100)
 print(ner_datasets['train'].features)
 print('2' * 100)
-print(ner_datasets['train'].features['entities'])
+label_list = ner_datasets['train'].features['ner_tags'].feature.names
 print('3' * 100)
-print(print(ner_datasets['train'][0]['text']))
+print(label_list)
 tokenizer = AutoTokenizer.from_pretrained('/home/nanji/workspace/chinese-macbert-base')
-print('4' * 100)
-print(tokenizer(ner_datasets['train'][0]['text']))
-res = tokenizer('interesting word is good')
-
-print(res)
-print('5' * 100)
-print(res.word_ids())
+a = tokenizer(ner_datasets['train'][0]['tokens'], is_split_into_words=True)
 
 
 def process_function(examples):
@@ -34,8 +28,16 @@ def process_function(examples):
         max_length=128,
         truncation=True
     )
-    for item in examples['entities']:
-        pass
+    tokenized_examples = tokenizer(examples['tokens'], max_length=128, truncation=True, is_split_into_words=True)
+    labels = []
+    for i, label in enumerate(examples['ner_tags']):
+        word_ids = tokenized_examples.word_ids(batch_index=i)
+        label_ids=[]
+        for word_id in word_ids:
+            pass
+
+
+
     return tokenizer_examples
 
 
